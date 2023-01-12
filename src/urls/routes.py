020257ab0535214d08ba648ccfe6e5425e967ea2
teddyrugged <jakpaetuk@gmail.com>
2023-01-12 23:44:28 +0100

    productview

--- src/urls/routes.py
+++ src/urls/routes.py
@@ -1,9 +1,26 @@
-from ..views.ordered import OrderedsApi, OrderedApi, OrderedSearchApi
-from ..views.orderedReview import OrderedReviewsApi, OrderedReviewApi, OrderedReviewSearchApi
-from ..views.orderProduct import OrderedProductsApi, OrderedProductApi, OrderedProductSearchApi
+from ..views.orderViews.ordered import OrderedsApi, OrderedApi, OrderedSearchApi
+from ..views.orderViews.orderedReview import OrderedReviewsApi, OrderedReviewApi, OrderedReviewSearchApi
+from ..views.orderViews.orderProduct import OrderedProductsApi, OrderedProductApi, OrderedProductSearchApi
+
+from ..views.productViews.product import ProductsApi, ProductApi, ProductSearchApi
+from ..views.productViews.productCategory import PrdouctCategoryApi, ProductCategoriesApi, PrdouctCategorySearchApi
+from ..views.productViews.productSubCategory import ProductSubCategoriesApi, ProductSubCategoryApi, ProductSubCategorySearchApi
 
 
 def initialize_routes(api):
+    api.add_resource(ProductsApi, "/api/products")
+    api.add_resource(ProductApi, "/api/product/<id>")
+    api.add_resource(ProductSearchApi, '/api/products/search/')
+
+    api.add_resource(ProductCategoriesApi, "/api/product_category")
+    api.add_resource(PrdouctCategoryApi, "/api/product_category/<id>")
+    api.add_resource(PrdouctCategorySearchApi, "/api/product_category/search/")
+
+    api.add_resource(ProductSubCategoriesApi, "/api/product_sub_category")
+    api.add_resource(ProductSubCategoryApi, "/api/product_sub_category/<id>")
+    api.add_resource(ProductSubCategorySearchApi, "/api/product_sub_category/search/")
+
+
     api.add_resource(OrderedsApi, "/api/ordered")
     api.add_resource(OrderedApi, "/api/ordered/<id>")
     api.add_resource(OrderedSearchApi, '/api/ordered/search/')
